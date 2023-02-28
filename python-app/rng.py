56e268e9285ea0fa7962c57e8c862a900aeb52fb
Duke <duke.letran@gmail.com>
2023-02-28 12:51:58 -0800

    added docker notes directly to python-app/rng.py

--- python-app/rng.py
+++ python-app/rng.py
@@ -1,10 +1,20 @@
+"""
+# Temporal Method
+docker build -t pythontest:latest .
+docker run --rm -it pythontest:latest  
+
+# 
+docker build -t pythontest:latest
+docker run --name pythoncontainer -it pythontest:latest  
+docker start -a -i pythoncontainer
+"""
 from random import randint
 
-min_number = int(input('Please enter the min number: '))
-max_number = int(input('Please enter the max number: '))
+min_number = int(input("Please enter the min number: "))
+max_number = int(input("Please enter the max number: "))
 
 if (max_number < min_number): 
-  print('Invalid input - shutting down...')
+  print('Max number is less min number. Invalid input...')
 else:
   rnd_number = randint(min_number, max_number)
   print(rnd_number)
